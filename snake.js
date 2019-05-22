c211bef9a1bff3777ec17a30d5d5c595e74b98b0
koppelos <remikoppel@gmail.com>
2019-05-22 11:33:37 +0200

    [SNAKE-7]

--- snake.js
+++ snake.js
@@ -2,6 +2,20 @@ const BOARD_X = 40;
 const BOARD_Y = 20;
 const MODULE_SIZE = 20;
 
+class SnakeBody
+{
+    constructor()
+    {
+        this.model=document.createElement("canvas");
+        this.ctx=ctx;
+        this.canvas=canvas;
+        this.bodyParts = 1;
+
+        this.fillStyle = 'rgb(244,12,45)';
+        ctx.fillRect(x,y,height,width);
+    };
+};
+
 class SnakeGame{
     constructor() {
         this.game = document.createElement('canvas');
@@ -15,3 +29,6 @@ class SnakeGame{
 const snake = new SnakeGame();
 
 window.addEventListener('DOMContentLoaded', () => document.body.appendChild(snake.game));
+
+
+
