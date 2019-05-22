ce9f561cc26d979259afb5c12b9225b45e4e6f7e
Mateusz Tecław <mateusz.teclaw@zut.edu.pl>
2019-05-22 11:03:05 +0200

    [SNAKE-MT] Prepare needed consts and sanitize canvas creation

--- snake.js
+++ snake.js
@@ -1,6 +1,17 @@
-class SnakeGame{
-    constructor(){}
+const BOARD_X = 40;
+const BOARD_Y = 20;
+const MODULE_SIZE = 20;
 
-    build(){};
-    render(){};
+class SnakeGame{
+    constructor() {
+        this.game = document.createElement('canvas');
+        this.game.width = (BOARD_X + 1) * MODULE_SIZE - 1;
+        this.game.height = (BOARD_Y + 1) * MODULE_SIZE - 1;
+    }
+    build() {};
+    render() { };
 }
+
+const snake = new SnakeGame();
+
+window.addEventListener('DOMContentLoaded', () => document.body.appendChild(snake.game));
