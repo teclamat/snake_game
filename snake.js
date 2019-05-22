b888a208f1507b5109906944e617dcb5677d5930
blusik99 <blusik999@outlook.com>
2019-05-22 11:39:29 +0200

    [SNAKE-13] I started to make loops

--- snake.js
+++ snake.js
@@ -9,7 +9,19 @@ class SnakeGame{
         this.game.height = BOARD_Y * (MODULE_SIZE + 1) - 1;
     }
     build() {};
-    render() {};
+    render() {
+
+        for( let i = 0; i < snake.length ; i++){
+            this.ctx.fillStyle = ( i == 0 )? "green" : "white";
+            this.ctx.fillRect(snake[i].x,snake[i].y,box,box);
+          
+            this.ctx.strokeStyle = "color";
+            this.ctx.strokeRect(snake[i].x,snake[i].y,box,box);
+        }
+      
+      this.ctx.fillStyle = "red";
+      this.ctx.fillRect(food.x,food.y,box,box);
+
 }
 
 const snake = new SnakeGame();
