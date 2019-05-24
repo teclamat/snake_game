6436e0aaf885e8f02ebcc583154f1579ba0277a6
kuro-kky <kurokky2020@gmail.com>
2019-05-24 14:10:32 +0200

    [SNAKE-6] added GameBoard class

--- snake.js
+++ snake.js
@@ -16,27 +16,30 @@ const snake = new SnakeGame();
 
 window.addEventListener('DOMContentLoaded', () => document.body.appendChild(snake.game));
 
-function DrawBoard(){
-    function DrawLine(x, y, d){
-        ctx = snake.game.getContext('2d');
+
+class GameBoard{
+    constructor(){}
+    DrawLine(x, y, d){
+        const ctx = snake.game.getContext('2d');
         this.d = d;
         ctx.strokeStyle = 'grey';
         ctx.moveTo(x, y);
-        if (d==0) {
-            ctx.lineTo(x, snake.game.height); 
-        } else if (d==1) {
-            ctx.lineTo(snake.game.width, y); 
-        }
+        if (d==0) { ctx.lineTo(x, snake.game.height); }
+        else if (d==1) { ctx.lineTo(snake.game.width, y); }
         ctx.lineWidth = 1; 
-        ctx.stroke(); 
+        ctx.stroke();
     }
-    for(var x = MODULE_SIZE, y = 0; x < snake.game.width; x +=MODULE_SIZE){
-        let direction = 0;
-        DrawLine(x, y, direction); 
-    }
-    for(var x = 0, y = MODULE_SIZE; y < snake.game.height; y +=MODULE_SIZE){
-        let direction = 1;
-        DrawLine(x, y, direction); 
-    }
+    DrawBoard(){
+        for(var x = MODULE_SIZE, y = 0; x < snake.game.width; x +=MODULE_SIZE){
+            let direction = 0;
+            this.DrawLine(x, y, direction); 
+        }
+        for(var x = 0, y = MODULE_SIZE; y < snake.game.height; y +=MODULE_SIZE){
+            let direction = 1;
+            this.DrawLine(x, y, direction); 
+        }
+   }   
 }
-DrawBoard();
+
+const board = new GameBoard();
+board.DrawBoard();
