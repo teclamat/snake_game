9495366361c1c98f3b99ec220c42a04da074d2b4
koppelos <remikoppel@gmail.com>
2019-05-30 11:39:47 +0200

    snek body module

--- snake.js
+++ snake.js
@@ -2,18 +2,53 @@ const BOARD_X = 40;
 const BOARD_Y = 20;
 const MODULE_SIZE = 20;
 
+class GameBoard {
+    constructor(gameCanvas, color) {
+        this.gameContext = gameCanvas.getContext('2d');
+        this.model = gameCanvas.cloneNode();
+        this.ctx = this.model.getContext('2d');
+        this.ctx.lineWidth = 1;
+        this.ctx.strokeStyle = color;
+        this.ctx.fillStyle = 'white';
+        this.build();
+    }
+    build() {
+        this.ctx.fillRect(0, 0, this.model.width, this.model.height);
+
+        for (let x = MODULE_SIZE + 1.5; x < this.model.width; x += MODULE_SIZE + 1) {
+            this.ctx.moveTo(x, 0);
+            this.ctx.lineTo(x, this.model.height);
+        }
+
+        for (let y = MODULE_SIZE + 1.5; y < this.model.height; y += MODULE_SIZE + 1) {
+            this.ctx.moveTo(0, y);
+            this.ctx.lineTo(this.model.width, y);
+        }
+
+        this.ctx.stroke();
+    }
+    draw() {
+        this.gameContext.drawImage(this.model, 0, 0);
+    }
+}
+
 class SnakeBody
 {
     constructor()
     {
         this.model=document.createElement("canvas");
-        this.ctx=ctx;
         this.canvas=canvas;
+        this.model.width = MODULE_SIZE;
+        this.model.height = MODULE_SIZE;
+        this.ctx = this.model.getContext('2d');
         this.bodyParts = 1;
-
-        this.fillStyle = 'rgb(244,12,45)';
-        ctx.fillRect(x,y,height,width);
-    };
+        this.build();
+    }
+    build()
+    {
+        this.ctx.fillStyle = "rgb(0,12,0)";
+        this.ctx.fillRect(0,0,MODULE_SIZE, MODULE_SIZE);
+    }
 };
 
 class SnakeGame{
@@ -21,14 +56,15 @@ class SnakeGame{
         this.game = document.createElement('canvas');
         this.game.width = BOARD_X * (MODULE_SIZE + 1) - 1;
         this.game.height = BOARD_Y * (MODULE_SIZE + 1) - 1;
+        this.board = new GameBoard(this.game, 'lightgray');
+        this.render();
     }
     build() {};
-    render() {};
+    render() {
+        this.board.draw();
+    };
 }
 
 const snake = new SnakeGame();
 
 window.addEventListener('DOMContentLoaded', () => document.body.appendChild(snake.game));
-
-
-
