abca22d5e03b150fcbe2539e91646273dede9736
Mateusz Tecław <mateusz.teclaw@zut.edu.pl>
2019-06-05 20:22:06 +0200

    [SNAKE-7] Added snake module

--- snake.js
+++ snake.js
@@ -2,7 +2,6 @@ const BOARD_X = 40;
 const BOARD_Y = 20;
 const MODULE_SIZE = 20;
 
-<<<<<<< HEAD
 class Wall {
     constructor(gameCanvas) {
         this.game = gameCanvas.getContext('2d');
@@ -13,8 +12,17 @@ class Wall {
         this.build();
     }
     build() {
-        this.ctx.fillStyle = 'red';
-        this.ctx.fillRect(0, 0, MODULE_SIZE, MODULE_SIZE);
+        this.ctx.fillStyle = 'grey';
+        this.ctx.strokeStyle = 'grey';
+        this.ctx.lineWidth = 4;
+
+        this.ctx.beginPath();
+        this.ctx.rect(0, 0, MODULE_SIZE, MODULE_SIZE);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();
+        this.ctx.rect(4, 4, MODULE_SIZE - 8, MODULE_SIZE - 8);
+        this.ctx.fill();
     }
     draw(x, y) {
         this.game.drawImage(this.model, x * (MODULE_SIZE + 1), y * (MODULE_SIZE + 1));
@@ -22,11 +30,8 @@ class Wall {
 }
 
 class Grid {
-=======
-class GameBoard {
->>>>>>> snek body module
     constructor(gameCanvas, color) {
-        this.gameContext = gameCanvas.getContext('2d');
+        this.game = gameCanvas.getContext('2d');
         this.model = gameCanvas.cloneNode();
         this.ctx = this.model.getContext('2d');
         this.ctx.lineWidth = 1;
@@ -37,20 +42,12 @@ class GameBoard {
     build() {
         this.ctx.fillRect(0, 0, this.model.width, this.model.height);
 
-<<<<<<< HEAD
         for (let x = MODULE_SIZE + 0.5; x < this.model.width; x += MODULE_SIZE + 1) {
-=======
-        for (let x = MODULE_SIZE + 1.5; x < this.model.width; x += MODULE_SIZE + 1) {
->>>>>>> snek body module
             this.ctx.moveTo(x, 0);
             this.ctx.lineTo(x, this.model.height);
         }
 
-<<<<<<< HEAD
         for (let y = MODULE_SIZE + 0.5; y < this.model.height; y += MODULE_SIZE + 1) {
-=======
-        for (let y = MODULE_SIZE + 1.5; y < this.model.height; y += MODULE_SIZE + 1) {
->>>>>>> snek body module
             this.ctx.moveTo(0, y);
             this.ctx.lineTo(this.model.width, y);
         }
@@ -58,49 +55,42 @@ class GameBoard {
         this.ctx.stroke();
     }
     draw() {
-        this.gameContext.drawImage(this.model, 0, 0);
+        this.game.drawImage(this.model, 0, 0);
     }
 }
 
-<<<<<<< HEAD
 class SnakeBody {
-    constructor() {
+    constructor(gameCanvas) {
+        this.game = gameCanvas.getContext('2d');
         this.model = document.createElement("canvas");
-        this.ctx = ctx;
-        this.canvas = canvas;
-        this.bodyParts = 1;
-
-        this.fillStyle = 'rgb(244,12,45)';
-        ctx.fillRect(x, y, height, width);
-    }
-}
-=======
-class SnakeBody
-{
-    constructor()
-    {
-        this.model=document.createElement("canvas");
-        this.canvas=canvas;
         this.model.width = MODULE_SIZE;
         this.model.height = MODULE_SIZE;
         this.ctx = this.model.getContext('2d');
-        this.bodyParts = 1;
         this.build();
     }
-    build()
-    {
-        this.ctx.fillStyle = "rgb(0,12,0)";
-        this.ctx.fillRect(0,0,MODULE_SIZE, MODULE_SIZE);
+    build() {
+        this.ctx.lineWidth = 4;
+
+        this.ctx.beginPath();
+        this.ctx.rect(0, 0, MODULE_SIZE, MODULE_SIZE);
+        this.ctx.stroke();
+
+        this.ctx.beginPath();
+        this.ctx.arc(MODULE_SIZE / 2, MODULE_SIZE / 2, MODULE_SIZE / 3, 0, Math.PI * 2);
+        this.ctx.fill();
     }
-};
->>>>>>> snek body module
+    draw(x, y) {
+        this.game.drawImage(this.model, x * (MODULE_SIZE + 1), y * (MODULE_SIZE + 1));
+    }
+}
 
 class SnakeGame {
     constructor() {
         this.game = document.createElement('canvas');
         this.game.width = BOARD_X * (MODULE_SIZE + 1) - 1;
         this.game.height = BOARD_Y * (MODULE_SIZE + 1) - 1;
-<<<<<<< HEAD
+        this.snake = [{ x: 10, y: 10 }, { x: 11, y: 10 }, { x: 12, y: 10 }];
+        this.snakeModule = new SnakeBody(this.game);
         this.grid = new Grid(this.game, 'lightgray');
         this.wall = new Wall(this.game);
         this.render();
@@ -109,16 +99,8 @@ class SnakeGame {
     render() {
         this.grid.draw();
         this.wall.draw(5, 6);
+        this.snake.forEach(coord => this.snakeModule.draw(coord.x, coord.y));
     }
-=======
-        this.board = new GameBoard(this.game, 'lightgray');
-        this.render();
-    }
-    build() {};
-    render() {
-        this.board.draw();
-    };
->>>>>>> snek body module
 }
 
 const snake = new SnakeGame();
