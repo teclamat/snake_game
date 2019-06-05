19497cd7568147110bd24ad958bf0d98cd7a215e
Mateusz Tecław <mateusz.teclaw@zut.edu.pl>
2019-06-05 23:53:43 +0200

    [SNAKE-12] Added collision with wall

--- snake.js
+++ snake.js
@@ -6,26 +6,34 @@ class Wall {
     constructor(gameCanvas) {
         this.game = gameCanvas.getContext('2d');
         this.model = document.createElement('canvas');
-        this.ctx = this.model.getContext('2d');
         this.model.width = MODULE_SIZE;
         this.model.height = MODULE_SIZE;
-        this.build();
+        this.hitModel = document.createElement('canvas');
+        this.hitModel.width = MODULE_SIZE;
+        this.hitModel.height = MODULE_SIZE;
+        this.build(this.model);
+        this.build(this.hitModel, '#a33');
     }
-    build() {
-        this.ctx.fillStyle = 'grey';
-        this.ctx.strokeStyle = 'grey';
-        this.ctx.lineWidth = 4;
+    build(model, color = 'grey') {
+        const ctx = model.getContext('2d');
+        ctx.fillStyle = color;
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 4;
 
-        this.ctx.beginPath();
-        this.ctx.rect(0, 0, MODULE_SIZE, MODULE_SIZE);
-        this.ctx.stroke();
+        ctx.beginPath();
+        ctx.rect(0, 0, MODULE_SIZE, MODULE_SIZE);
+        ctx.stroke();
 
-        this.ctx.beginPath();
-        this.ctx.rect(4, 4, MODULE_SIZE - 8, MODULE_SIZE - 8);
-        this.ctx.fill();
+        ctx.beginPath();
+        ctx.rect(4, 4, MODULE_SIZE - 8, MODULE_SIZE - 8);
+        ctx.fill();
     }
-    draw(x, y) {
-        this.game.drawImage(this.model, x * (MODULE_SIZE + 1), y * (MODULE_SIZE + 1));
+    draw(x, y, hit = false) {
+        if (hit) {
+            this.game.drawImage(this.hitModel, x * (MODULE_SIZE + 1), y * (MODULE_SIZE + 1));
+        } else {
+            this.game.drawImage(this.model, x * (MODULE_SIZE + 1), y * (MODULE_SIZE + 1));
+        }
     }
 }
 
@@ -90,13 +98,20 @@ class SnakeGame {
         this.game.width = BOARD_X * (MODULE_SIZE + 1) - 1;
         this.game.height = BOARD_Y * (MODULE_SIZE + 1) - 1;
         this.snake = [{ x: 10, y: 10 }, { x: 11, y: 10 }, { x: 12, y: 10 }];
+        this.walls = this.wallCoords;
         this.snakeModule = new SnakeBody(this.game);
         this.grid = new Grid(this.game, 'lightgray');
         this.wall = new Wall(this.game);
+        this.gameEnded = false;
         this.moveDir = { x: 1, y: 0 };
         this.moveChange = null;
         window.addEventListener('keydown', ev => this.input(ev));
         this.render();
+    }
+    get wallCoords() {
+        return new Array(20).fill(null).map(() =>
+            new Object({ x: parseInt(Math.random() * (BOARD_X - 1)), y: parseInt(Math.random() * (BOARD_Y - 1)) })
+        );
     }
     render() {
         if (this.moveChange) {
@@ -105,9 +120,9 @@ class SnakeGame {
         }
         this.move();
         this.grid.draw();
-        this.wall.draw(5, 6);
+        this.walls.forEach(coord => this.wall.draw(coord.x, coord.y, coord.hit));
         this.snake.forEach(coord => this.snakeModule.draw(coord.x, coord.y));
-        setTimeout(() => this.render(), 300);
+        if (!this.gameEnded) setTimeout(() => this.render(), 100);
     }
     move() {
         const lastElem = this.snake[this.snake.length - 1];
@@ -116,8 +131,19 @@ class SnakeGame {
         if (newElem.x < 0) newElem.x = BOARD_X - 1;
         if (newElem.y >= BOARD_Y) newElem.y = 0;
         if (newElem.y < 0) newElem.y = BOARD_Y - 1;
+        this.gameEnded = this.collision(newElem);
+        if (this.gameEnded) return;
         this.snake.shift();
         this.snake.push(newElem);
+    }
+    collision(newSnakePos) {
+        for (let i = 0; i < this.walls.length; ++i) {
+            if (newSnakePos.x == this.walls[i].x && newSnakePos.y == this.walls[i].y) {
+                this.walls[i].hit = true;
+                return true;
+            }
+        }
+        return false;
     }
     input(ev) {
         switch (ev.key) {
